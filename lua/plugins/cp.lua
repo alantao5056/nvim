af8d8a2a161315aba06c37a9c6ce74662eff4896
alantao5056 <alantaolr@outlook.com>
2026-02-04 22:28:23 -0500

    testcase naming format change

--- lua/plugins/cp.lua
+++ lua/plugins/cp.lua
@@ -2,6 +2,16 @@ return {
   'xeluxee/competitest.nvim',
   dependencies = 'MunifTanjim/nui.nvim',
   config = function()
-    require('competitest').setup({})
+    require('competitest').setup({
+      testcases_use_single_file = false,
+
+      -- naming scheme
+      testcases_input_file_format = "$(TCNUM).in",
+      testcases_output_file_format = "$(TCNUM).out",
+
+      -- optional but useful
+      save_current_file = true,
+      evaluate_template_modifiers = true,
+    })
   end,
 }
